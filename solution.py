52c64b9506e13bbd1d809c15ac76df6f0b4a5872
ccTbsRWE <130173273+ccTbsRWE@users.noreply.github.com>
2023-04-13 23:48:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,24 @@
-import pandas as pd
+from scipy.stats import beta
 import numpy as np
+from calc_prob import calc_prob_between
 
+chat_id = 611202811 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(convs_ctrl: int, 
+             imps_ctrl: int, 
+             imps_test: int, 
+             convs_test: int) -> bool:
+    a_C, b_C = convs_ctrl+1, imps_ctrl-convs_ctrl+1
+    beta_C = beta(a_C, b_C)
+    a_T, b_T = convs_test+1, imps_test-convs_test+1
+    beta_T = beta(a_T, b_T)
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    #calculating the lift
+    lift=(beta_T.mean()-beta_C.mean())/beta_C.mean()
+
+    #calculating the probability for Test to be better than Control
+    prob=calc_prob_between(beta_T, beta_C)
+    if prob <= 0.07:
+      return False
+    else:
+      return True
